92ad1d36bc744609873031c4ebe9c6cfabed0e22
em <>
2021-08-04 17:44:09 +0200

    updates for 2021.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,27 @@
-import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 import org.jetbrains.intellij.tasks.PrepareSandboxTask
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.io.ByteArrayOutputStream
-import java.nio.charset.StandardCharsets
 
 plugins {
-    kotlin("jvm") version "1.4.10"
-    id("org.jetbrains.intellij") version "0.7.2"
+    id("java")
+//    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.intellij") version "1.1.4"
     id("net.researchgate.release") version "2.8.1"
 }
 
 group = "org.gap.ijplugins.spring.ideaspringtools"
 
-if(version.toString().endsWith("SNAPSHOT")) {
+if (version.toString().endsWith("SNAPSHOT")) {
     version = version.toString().replace("SNAPSHOT", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HH.mm.ss.SSS")))
 }
 
 repositories {
-    maven ("https://repo.huaweicloud.com/repository/maven")
+    maven("https://repo.huaweicloud.com/repository/maven")
     mavenCentral()
-//    mavenLocal()
-    maven ("https://jitpack.io")
-    // maven ("https://dl.bintray.com/gayanper/maven")
-    maven ("https://repo.spring.io/libs-snapshot/")
+    maven("https://jitpack.io")
+    maven("https://repo.spring.io/libs-snapshot/")
 }
 
 val languageServer by configurations.creating
@@ -31,29 +29,29 @@ val languageServer by configurations.creating
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.github.ballerina-platform:lsp4intellij:master-SNAPSHOT")
-    //implementation("com.github.ballerina-platform:lsp4intellij:0.94.2")
-    //implementation("com.github.ballerina-platform:lsp4intellij:0.94.1-20201108.10.09.08.085")
-
-    implementation("org.springframework.ide.vscode:commons-java:1.26.0-SNAPSHOT")
-    languageServer("org.springframework.ide.vscode:spring-boot-language-server:1.26.0-SNAPSHOT:exec") {
+    implementation("org.springframework.ide.vscode:commons-java:1.27.0-SNAPSHOT")
+    languageServer("org.springframework.ide.vscode:spring-boot-language-server:1.27.0-SNAPSHOT:exec") {
         isTransitive = false
     }
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "IC-2020.3"
-    pluginName = "idea-spring-tools"
-    setPlugins("IntelliLang", "java")
-    jreRepo = "https://jetbrains.bintray.com/intellij-jbr"
-    downloadSources = false
+    pluginName.set("idea-spring-tools")
+    version.set("2021.2")
+    type.set("IC")
+    downloadSources.set(true)
+    updateSinceUntilBuild.set(true)
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set("IntelliLang,java".split(',').map(String::trim).filter(String::isNotEmpty))
 }
 
 tasks {
     compileJava {
         sourceCompatibility = "11"
         targetCompatibility = "11"
-        options.encoding = StandardCharsets.UTF_8.name()
+//        options.encoding = StandardCharsets.UTF_8.name()
     }
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
@@ -61,19 +59,19 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+    patchPluginXml {
+        version.set("1.2.7")
+        sinceBuild.set("212")
+        untilBuild.set("213.*")
+    }
 }
 
-tasks.buildSearchableOptions {
-    enabled = false
-}
-
-tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
-    setUntilBuild("211.*")
-    setSinceBuild("191.*")
-}
+//tasks.buildSearchableOptions {
+//    enabled = false
+//}
 
 tasks.getByName<PrepareSandboxTask>("prepareSandbox").doLast {
-    val pluginServerDir = "${intellij.sandboxDirectory}/plugins/${intellij.pluginName}/lib/server"
+    val pluginServerDir = "${intellij.sandboxDir}/plugins/${intellij.pluginName}/lib/server"
 
     mkdir(pluginServerDir)
     copy {
@@ -83,7 +81,6 @@ tasks.getByName<PrepareSandboxTask>("prepareSandbox").doLast {
     }
 }
 
-
 tasks {
     buildPlugin {
         doLast() {
@@ -91,7 +88,7 @@ tasks {
                 <?xml version="1.0" encoding="UTF-8"?>
                 <plugins>
                     <plugin id="org.gap.ijplugins.spring.idea-spring-tools" version="${archiveVersion}">
-                        <idea-version since-build="191.8026.42" until-build="213.*" />
+                        <idea-version since-build="212.4746.92" until-build="213.*" />
                     </plugin>
                 </plugins>                
                 
@@ -109,16 +106,16 @@ release {
     buildTasks = arrayListOf("buildPlugin")
 }
 
-tasks {
-    afterReleaseBuild {
-        dependsOn("publishPlugin")
-    }
-
-    publishPlugin {
-        setToken(System.getenv("JB_API_KEY"))
-    }
-
-    runIde {
-        setJvmArgs(listOf("-Dsts4.jvmargs=-Xmx512m -Xms512m"))
-    }
-}
+//tasks {
+//    afterReleaseBuild {
+//        dependsOn("publishPlugin")
+//    }
+//
+//    publishPlugin {
+//        token.set(System.getenv("JB_API_KEY"))
+//    }
+//
+//    runIde {
+//        setJvmArgs(listOf("-Dsts4.jvmargs=-Xmx512m -Xms512m"))
+//    }
+//}
